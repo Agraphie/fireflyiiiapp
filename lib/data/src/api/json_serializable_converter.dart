b784f78b34d81e9c7e571992413a4193f28b3a03
Clemens Keppler <clemens.keppler@rwth-aachen.de>
2020-04-30 11:45:37 +0200

    Add missing json converter

---
+++ lib/data/src/api/json_serializable_converter.dart
@@ -0,0 +1,48 @@
+import 'package:chopper/chopper.dart';
+import 'package:fireflyapp/data/src/api/http_error.dart';
+
+typedef T JsonFactory<T>(Map<String, dynamic> json);
+
+class JsonSerializableConverter extends JsonConverter {
+  final Map<Type, JsonFactory> factories;
+
+  JsonSerializableConverter(this.factories);
+
+  T _decodeMap<T>(Map<String, dynamic> values) {
+    /// Get jsonFactory using Type parameters
+    /// if not found or invalid, throw error or return null
+    final jsonFactory = factories[T];
+    if (jsonFactory == null || jsonFactory is! JsonFactory<T>) {
+      /// throw serializer not found error;
+      return null;
+    }
+
+    return jsonFactory(values) as T;
+  }
+
+  List<T> _decodeList<T>(Iterable values) =>
+      values.where((dynamic v) => v != null).map<T>(_decode).toList();
+
+  dynamic _decode<T>(dynamic entity) {
+    if (entity is Iterable) return _decodeList<T>(entity);
+
+    if (entity is Map) return _decodeMap<T>(entity);
+
+    return entity;
+  }
+
+  @override
+  Response<ResultType> convertResponse<ResultType, Item>(Response response) {
+    if (ResultType is String) {
+      return response.body as Response<ResultType>;
+    }
+    // use [JsonConverter] to decode json
+    final jsonRes = super.convertResponse(response);
+
+    return jsonRes.copyWith<ResultType>(body: _decode<Item>(jsonRes.body));
+  }
+
+  @override
+  // all objects should implements toJson method
+  Request convertRequest(Request request) => super.convertRequest(request);
+}
